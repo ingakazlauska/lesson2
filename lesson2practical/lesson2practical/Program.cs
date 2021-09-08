24149a489fe9eb96fb3d3d38070d5dd8669c300d
inga.kazlauska <inga.kazlauska@gmail.com>
2021-09-08 22:10:21 +0300

    Changed

--- lesson2practical/lesson2practical/Program.cs
+++ lesson2practical/lesson2practical/Program.cs
@@ -6,19 +6,44 @@ namespace lesson2practical
     {
         static void Main(string[] args)
         {
-           
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
+
+
+            //Console.WriteLine("Kā Tevi sauc?");
+
+            //string userName = Console.ReadLine();
+
+            //Console.WriteLine("Sveiks, " + userName + "!");
+
+
+            // --------------------------------------
+
+
+
+            Console.WriteLine("Kāds ir Tavs vecums?");
+
+            string ageText = Console.ReadLine();
+
+
+            int age = int.Parse(ageText);
+            int previosusYearAge = age + 1;
+
+            Console.WriteLine("Sveiks, Tev nākošgad būs" + previosusYearAge); 
+
+
+        
+            bool isAdult = false;
+            Console.WriteLine("Tavs vecums ir " + "Tu esi pilngadīgs " + isAdult);
+
+
+
+
+
+
+
+
+
+
+
             Console.WriteLine("Hello World!");
         }
     }
