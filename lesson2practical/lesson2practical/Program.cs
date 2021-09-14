0fb523ead9b55d5a19ca76661f94e1840909035a
inga.kazlauska <inga.kazlauska@gmail.com>
2021-09-14 23:24:10 +0300

    Homework

--- lesson2practical/lesson2practical/Program.cs
+++ lesson2practical/lesson2practical/Program.cs
@@ -19,32 +19,153 @@ namespace lesson2practical
 
 
 
+            //Console.WriteLine("Kāds ir Tavs vecums?");
+
+            //string ageText = Console.ReadLine();
+
+
+            //int age = int.Parse(ageText);
+            //int NextYearAge = age + 1;
+
+            //Console.WriteLine("Nākamgad Tev paliks " + NextYearAge + " ,Tu esi pilngadīgs!");
+
+            //---------------------------------
+
+            //Console.WriteLine("Ieraksti skaitli!");
+
+            //string your1stnumberis = Console.ReadLine();
+            //int number1 = int.Parse(your1stnumberis);
+
+            //Console.WriteLine("Ieraksti vēl vienu skaitli!");
+
+            //string your2ndnumberis = Console.ReadLine();
+            //int number2 = int.Parse(your2ndnumberis);
+
+            //int maxnumber = Math.Max(number1, number2);
+
+
+            //Console.WriteLine("Lielākais skaitlis ir " + maxnumber);
+
+            //-------------------------------------------------------------
+
+            //Console.WriteLine("Ieraksti skaitli!");
+
+            //string your1stnumber = Console.ReadLine();
+            //int number11 = int.Parse(your1stnumber);
+
+            //Console.WriteLine("Ieraksti vēl vienu skaitli!");
+
+            //string your2ndnumber = Console.ReadLine();
+            //int number22 = int.Parse(your2ndnumber);
+
+            //int minnumber = Math.Min(number11, number22);
+
+
+            //Console.WriteLine("Mazākais skaitlis ir " + minnumber);
+
+            //----------------------------------------------------------
+
+            //Console.WriteLine("Ieraksti skaitli!");
+
+            //string your1number = Console.ReadLine();
+            //int number111 = int.Parse(your1number);
+
+            //Console.WriteLine("Ieraksti vēl vienu skaitli!");
+
+            //string your2number = Console.ReadLine();
+            //int number222 = int.Parse(your2number);
+
+            //int result = number111 % number222;
+
+
+            //Console.WriteLine("Dalījuma atlikums ir " + result);
+
+            //----------------------------------------------
+
+            //Console.WriteLine("Ieraksti skaitli!");
+
+            //string numberPlease = Console.ReadLine();
+            //int numberAgain = int.Parse(numberPlease);
+
+
+            //if (numberAgain % 2 == 0)
+
+            //    Console.WriteLine("Pāra skaitlis");
+
+            //else
+
+            //    Console.WriteLine("Nepāra skaitlis");
+
+            //--------------------------------------------------
+
+            //Console.WriteLine("Kāds ir taisnstūra īsākās malas garums centimetros?");
+
+            //string edge1 = Console.ReadLine();
+            //int edge11 = int.Parse(edge1);
+
+            //Console.WriteLine("Kāds ir taisnstūra garākās malas garums centimetros?");
+
+            //string edge2 = Console.ReadLine();
+            //int edge22 = int.Parse(edge2);
+
+            //double result3 = edge11 * edge22;
+            //double rounded = Math.Round(result3, 2);
+
+            //Console.WriteLine("taisnstūra laukums ir " + rounded  + "cm2.");
+
+
+            //-----------------------
+
+            //Console.WriteLine("Kāds ir trijstura katetes  garums centimetros?");
+
+            //string tredge1 = Console.ReadLine();
+            //int tredge11 = int.Parse(tredge1);
+
+            //Console.WriteLine("Kāds ir trijstura otras katetes  garums centimetros?");
+
+            //string tredge2 = Console.ReadLine();
+            //int tredge22 = int.Parse(tredge2);
+
+            //double result4 = tredge11 * tredge22/2;
+
+
+            //Console.WriteLine("Trijstūra laukums ir " + result4 + "cm2.");
+
+            //-------------------------------------------------
+
+
+            Console.WriteLine("Kā Tevi sauc?");
+
+            string userName1 = Console.ReadLine();
+
+    
+
             Console.WriteLine("Kāds ir Tavs vecums?");
 
-            string ageText = Console.ReadLine();
+            string ageText1 = Console.ReadLine();
 
 
-            int age = int.Parse(ageText);
-            int previosusYearAge = age + 1;
-
-            Console.WriteLine("Sveiks, Tev nākošgad būs" + previosusYearAge); 
+            Console.WriteLine($"Sveiks, {userName1}, tavs vecums ir {ageText1}!");
 
 
-        
-            bool isAdult = false;
-            Console.WriteLine("Tavs vecums ir " + "Tu esi pilngadīgs " + isAdult);
-
-
-
-
-
-
-
-
-
-
-
-            Console.WriteLine("Hello World!");
         }
     }
-}
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
